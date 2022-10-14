6c732a880e069338600768a4adc7b9038884a77d
Maksim <94144650+410867@users.noreply.github.com>
2022-10-14 19:34:50 +0300

    Update index.js

--- Scripts/index.js
+++ Scripts/index.js
@@ -4,6 +4,6 @@ const thirdNumberUser = Number(prompt('Введіть третє число: '))
 const arithmeticMean = (firstNumberUser + secondNumberUser + thirdNumberUser) / 3;
 
 if(((arithmeticMean * 3) % 3) === 0)
-    alert('Середнє арифметичне трьох введених чисел: ' + arithmeticMean);
+    alert(`Середнє арифметичне трьох введених чисел: ${arithmeticMean}`);
 else
-    alert('Середнє арифметичне трьох введених чисел: ' + arithmeticMean.toFixed(3));
+    alert(`Середнє арифметичне трьох введених чисел: ${arithmeticMean.toFixed(3)}`);
